5db770f4e4d7916a458648e5a7ec9072b96e2337
Steve Brunton <brunton@dudeman.com>
2016-12-14 23:09:40 -0500

    attempting to finish up the plugin stuff

--- gcs/index.php
+++ gcs/index.php
@@ -26,10 +26,7 @@ Short Name: gcs
   }
 
   function gcs_upload($resource) {
-    $storage = new StorageClient([
-      'projectId' => osc_get_preference('project', 'gcs')
-    ]);
-    $bucket = $storage->bucket(osc_get_preference('project', 'gcs'));
+    $bucket = get_gcs_bucket();
     if (osc_keep_original_image()) {
       $bucket->upload(fopen(osc_base_path() . $resource['s_path'] . $resource['pk_i_id'] . '_original.' . $resource['s_extension'], 'r'));
     }
@@ -39,13 +36,88 @@ Short Name: gcs
     gcs_unlink_resource($resource);
   }
 
+  # http://storage.googleapis.com/uspto-pair/applications/05900002.zip
+  function gcs_resource_path($path) {
+    return "http://storage.googleapis.com/". osc_get_preference('bucket', 'gcs') ."/". str_replace(osc_base_url().osc_resource_field("s_path"), '', $path);
+  }
+
+  function get_gcs_bucket() {
+    $storage = new StorageClient([
+      'projectId' => osc_get_preference('project', 'gcs')
+    ]);
+    return $storage->bucket(osc_get_preference('bucket', 'gcs'));
+  }
+
+  function gcs_regenerate_image($resource) {
+    $bucket = get_gcs_bucket();
+    $path = $resource['pk_id_id']. "_original" . $resource['s_extenson'];
+    $img = $bucket->object($path);
+    if(!$img.exists()) {
+      $path = $resource['pk_i_id']. "." . $resource['s_extension'];
+      $img = $bucket->object($path);
+    }
+    if(!$img.exists()) {
+      $path = $resource['pk_i_id']. "_thumbnail." . $resource['s_extension'];
+      $img = $bucket->object($path);
+    }
+    if($img.exists()) {
+      $stream = $img->downloadToFile(osc_base_path() . $resource['s_path'] . $resource['pk_i_id'] . "." . $resource['s_extension']);
+      $i = $bucket->object($resource['pk_i_id']. "_original." . $resource['s_extension']);
+      $i->delete();
+      $i = $bucket->object($resource['pk_i_id']. "." . $resource['s_extension']);
+      $i->delete();
+      $i = $bucket->object($resource['pk_i_id']. "_preview." . $resource['s_extension']);
+      $i->delete();
+      $i = $bucket->object($resource['pk_i_id']. "_thumbnail." . $resource['s_extension']);
+      $i->delete();
+    }
+  }
+
   function gcs_unlink_resource($resource) {
       @unlink(osc_base_path() . $resource['s_path'] . $resource['pk_i_id'] . '_original.' . $resource['s_extension']);
       @unlink(osc_base_path() . $resource['s_path'] . $resource['pk_i_id'] . '.' . $resource['s_extension']);
       @unlink(osc_base_path() . $resource['s_path'] . $resource['pk_i_id'] . '_preview.' . $resource['s_extension']);
       @unlink(osc_base_path() . $resource['s_path'] . $resource['pk_i_id'] . '_thumbnail.' . $resource['s_extension']);
   }
-# http://storage.googleapis.com/uspto-pair/applications/05900002.zip
+
+  function gcs_delete_from_bucket($resource) {
+    $bucket = get_gcs_bucket();
+    $i = $bucket->object($resource['pk_i_id']. "_original." . $resource['s_extension']);
+    $i->delete();
+    $i = $bucket->object($resource['pk_i_id']. "." . $resource['s_extension']);
+    $i->delete();
+    $i = $bucket->object($resource['pk_i_id']. "_preview." . $resource['s_extension']);
+    $i->delete();
+    $i = $bucket->object($resource['pk_i_id']. "_thumbnail." . $resource['s_extension']);
+    $i->delete();
+  }
+
+  function gcs_admin_menu() {
+    if (osc_version()<320) {
+      echo '<h3><a href="#">Google Cloud Storage</a></h3>
+      <ul> 
+          <li><a href="' . osc_admin_render_plugin_url(osc_plugin_folder(__FILE__) . 'admin/conf.php') . '">&raquo; ' . __('Settings', 'gcs') . '</a></li>
+      </ul>';
+    } else {
+      osc_add_admin_submenu_divider('plugins', 'Google Cloud Storage plugin', 'gcs_divider', 'administrator');
+      osc_add_admin_submenu_page('plugins', __('Google Cloud Storage options', 'gcs'), osc_route_admin_url('gcs-admin-conf'), 'gcs_settings', 'administrator');
+    }
+  }
+
+  function gcs_configure_link() {
+    if(osc_version()<320) {
+        osc_redirect_to(osc_admin_render_plugin_url(osc_plugin_folder(__FILE__)).'admin/conf.php');
+    } else {
+        osc_redirect_to(osc_route_admin_url('gcs-admin-conf'));
+    }
+  }
+    
+  if(osc_version()>=320) {
+      /**
+        * ADD ROUTES (VERSION 3.2+)
+        */
+      osc_add_route('gcs-admin-conf', 'gcs/admin/conf', 'gcs/admin/conf', osc_plugin_folder(__FILE__).'admin/conf.php');
+  }      
 
   osc_register_plugin(osc_plugin_path(__FILE__), 'gcs_install');
   osc_add_hook(osc_plugin_path(__FILE__)."_uninstall", 'gcs_uninstall');
